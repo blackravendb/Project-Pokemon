d389481de8de075e45270d5313c5879b6b1b4d83
Dennis Brandmüller <dennis. brandmueller@stud.fh-rosenheim.de>
2013-04-25 23:19:51 +0200

    second commit

--- Project-Pokemon/src/main.java
+++ Project-Pokemon/src/main.java
@@ -5,6 +5,7 @@ public class main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("ON/OFF - Soll ich da draufdrücken oder was?");
 		System.out.println("DIE SCHEISSE GEHT!");
 	}
 
