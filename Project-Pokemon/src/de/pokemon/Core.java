33bba89b465cd3844e830583de580c2c0b3a3051
Dennis Brandmüller <dennis. brandmueller@stud.fh-rosenheim.de>
2013-04-27 00:33:03 +0200

    -adding some comments

--- Project-Pokemon/src/de/pokemon/Core.java
+++ Project-Pokemon/src/de/pokemon/Core.java
@@ -10,22 +10,22 @@ public class Core extends Applet implements Runnable {
 
 	private static final long serialVersionUID = 1L;
 
-	private static JFrame frame;
+	private static JFrame frame; //Hauptfenster des Spiels
 	
-	public static final int res = 1;
-	public static double oY = 0, oX = 0;
-	public static int dir = 0;
+	public static final int res = 1; //resolution: multiplikator für Bildpunkt * res = Pixelgröße auf Bildschirm
+	public static double oY = 0, oX = 0; //offset y und offset X
+	public static int dir = 0; //Richtungsvariable (0=hoch, 1=rechts, 2=runter, 3=links)
 	
-	public static boolean moving = false;
-	public static boolean run = false;
+	public static boolean moving = false; //Variabel ob Bewegung möglich ist (z.B. wärend Dialogen)
+	public static boolean run = false; //Ob der Charakter im moment Rennt (fraglich ob diese funktion überhaupt implementiert wird xD)
 	
 	private Image screen;
 	
-	public static Dimension screenSize = new Dimension (600, 500);
+	public static Dimension screenSize = new Dimension (600, 500); //Auflösung des Hauptfensters (muss noch besprochen werden)
 	public static Dimension pixel = new Dimension (screenSize.width, screenSize.height);
 	public static Dimension size;
 	
-	public static String name = "Project Pokemon";
+	public static String name = "Project Pokemon"; //Name des Hauptfensters
 	
 	public Core () {
 		setPreferredSize(screenSize);
@@ -46,9 +46,9 @@ public class Core extends Applet implements Runnable {
 		size = new Dimension(frame.getWidth(), frame.getHeight());
 		
 		frame.setTitle(name);
-		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false); //Verhindern, dass der Benutzer die Größe des Bildschirms ändern kann
+		frame.setLocationRelativeTo(null); //Zentrieren des Fensters in der Mitte des Bildschirms
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Wenn Fenster geschlossen wird, wird dadurch auch das Programm geschlossen
 		frame.setVisible(true);
 		core.start();
 
