0a1d5db59d4d01d5275e85042d25c8c8ca2e12b1
Lukas Schmitt <lukas.a.schmitt@gmail.com>
2013-06-06 14:42:51 +0200

    new Intro State

---
+++ Project-Pokemon/src/de/pokemon/IntroState.java
@@ -0,0 +1,44 @@
+package de.pokemon;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class IntroState extends BasicGameState {
+
+	public static int ID;
+	
+	public IntroState(int id){
+		ID = id;
+	}
+	
+	@Override
+	public void init(GameContainer container, StateBasedGame game)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(GameContainer container, StateBasedGame game, Graphics g)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer container, StateBasedGame game, int delta)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return ID;
+	}
+
+}
