40f1fb0b4c95acadfb2da71139c0104bc0dfeaba
Dennis Brandmüller <dennis. brandmueller@stud.fh-rosenheim.de>
2013-06-06 13:41:23 +0200

    Erste Gehversuche nach links (It's not a bug, its a feature)

--- Project-Pokemon/src/de/pokemon/Entity.java
+++ Project-Pokemon/src/de/pokemon/Entity.java
@@ -1,5 +1,6 @@
 package de.pokemon;
 
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
@@ -12,13 +13,19 @@ public class Entity {
 	private int height;
 	private int tileX;
 	private int tileY;
-	private static LastDir lastDir; /** Angabe ueber letzte Bewegungsrichtung, wobei 0 = down, 1 = right, 2 = top, 3 = left entspricht */
+	private static LastDir lastDir = LastDir.DOWN; /** Angabe ueber letzte Bewegungsrichtung, wobei 0 = down, 1 = right, 2 = top, 3 = left entspricht */
 	
 	private enum LastDir {DOWN, LEFT, UP, RIGHT};
 	
 	private Image image;
 	
 	private Boolean isRunning = false;
+	
+	private Image charImages[][] = new Image[4][3];
+	private Animation aniLeft;
+	private Animation aniUp = new Animation();
+	private Animation aniRight = new Animation();
+	private Animation aniDown = new Animation();;
 	
 	Entity (int posX, int posY, int width, int height, String imagePath) throws SlickException{
 		this.posX = posX;
@@ -33,6 +40,15 @@ public class Entity {
 
 		if(tileX % Core.tileSize == 0)
 			tileX %= Core.tileSize;
+		
+		//Animationsbilder laden
+		for(int i = 0; i < 4; i++){
+			for (int j = 0; j < 3; j++){
+				charImages[i][j]= image.getSubImage(i*width, j*height, width, height);
+			}
+		}
+		aniLeft = new Animation(charImages[0],200);
+		aniLeft.setPingPong(true);
 
 	}
 	
@@ -92,11 +108,24 @@ public class Entity {
 		//TODO
 		if(isRunning){
 			//Bewegungsanimation
+			if(lastDir == LastDir.LEFT){
+				aniLeft.draw(posX, posY);
+			}
+			else if(lastDir == LastDir.UP){
+				
+			}
+			else if(lastDir == LastDir.RIGHT){
+				
+			}
+			else if(lastDir == LastDir.DOWN){
+				
+				
+			}
 		}
 		else{
 			//Standanimation
 			if(lastDir.equals(LastDir.LEFT)){
-				image.getSubImage(0, 3*height, width, height).draw(posX,posY);
+				image.getSubImage(0, 2*height, width, height).draw(posX,posY);
 			}
 			else if (lastDir.equals(LastDir.UP)){
 				image.getSubImage(0, 0, width, height).draw(posX,posY);
@@ -105,31 +134,34 @@ public class Entity {
 				image.getSubImage(1*width, 0, width, height).draw(posX,posY);
 			}
 			else if (lastDir.equals(LastDir.DOWN)){
-				image.getSubImage(3*width, 1*height, width, height).draw(posX,posY);
+				image.getSubImage(2* width, 1*height, width, height).draw(posX,posY);
 			}
 		}
-		image.getSubImage(0, 0, width, height).draw(posX,posY);
 	}
 	
 	public void updateEntity (Input input){
 		if(input.isKeyDown(Input.KEY_A)){
 			lastDir = LastDir.LEFT;
 			isRunning = true;
+			posX -= 1;
 		}
 		
 		else if(input.isKeyDown(Input.KEY_W)){
 			lastDir = LastDir.UP;
 			isRunning = true;
+			posY -= 1;
 		}
 		
 		else if(input.isKeyDown(Input.KEY_D)){
 			lastDir = LastDir.RIGHT;
 			isRunning = true;
+			posX += 1;
 		}
 		
 		else if(input.isKeyDown(Input.KEY_S)){
 			lastDir = LastDir.DOWN;
 			isRunning = true;
+			posY += 1;
 		}
 		else{
 			isRunning = false;
