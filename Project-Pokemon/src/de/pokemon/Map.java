a7d45ee84e85c670fa3355c6f0ba7f90191add2e
Lukas Schmitt <lukas.a.schmitt@gmail.com>
2013-06-09 20:31:57 +0200

    minor map changes

--- Project-Pokemon/src/de/pokemon/Map.java
+++ Project-Pokemon/src/de/pokemon/Map.java
@@ -34,7 +34,7 @@ public class Map extends TiledMapPlus{
 		blocked = buildCollisionMap();
 		tileSize = getTileWidth();
 		createWater();
-		System.out.println(this.getObjectGroup("object layer").objects.get(5).name);
+		//System.out.println(this.getObjectGroup("object layer").objects.get(5).name);
 		//System.out.println(blocked[24].length);
 	}
 
