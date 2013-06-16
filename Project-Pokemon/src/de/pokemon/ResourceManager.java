567a54e332f1ea8b113f677ffadd6e68534ebf31
Dennis Brandmüller <dennis. brandmueller@stud.fh-rosenheim.de>
2013-06-16 23:09:22 +0200

    - einige Npc's hinzugefügt - town map leicht abgeändert

--- Project-Pokemon/src/de/pokemon/ResourceManager.java
+++ Project-Pokemon/src/de/pokemon/ResourceManager.java
@@ -15,10 +15,12 @@ public class ResourceManager {
 	
 	public static void init(){
 		npcDatabase = new HashMap<String, NpcStruct>();
-		
-	//	npcDatabase.put("mom", new NpcStruct("mom", "res/npc1.png", new int[][]{{5,0,4,0,7,0,4},{4,200,0,200,4,200,0}}));
+	
 		npcDatabase.put("mom", new NpcStruct("mom", "res/npc1.png", new int[][]{{5,4},{0,1000},{4,0},{0,500},{7,4},{0,1000},{4,0},{0,500}}));
-		
+		npcDatabase.put("supervisor", new NpcStruct("supervisor", "res/supervisor.png", new int[][]{{3,0},{0,3000},{2,0},{0,3000}}));
+		npcDatabase.put("biker", new NpcStruct("biker", "res/biker.png", new int[][]{{6,3},{5,4},{8,3},{7,4}}));
+		npcDatabase.put("captain", new NpcStruct("captain", "res/captain.png", new int[][]{{3,0},{0,2147483647}}));
+		npcDatabase.put("deubler1", new NpcStruct("deubler1", "res/deubler.png", new int[][]{{1,0},{0,1000},{4,0},{0,1000},{3,0},{0,1000},{4,0},{0,1000}}));
 	}
 	
 	public static String getNpcImagePath(String name){
