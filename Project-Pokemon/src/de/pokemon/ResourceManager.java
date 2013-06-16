4f7b54f2996e738649f4f3b88692811b58deecf5
Dennis Brandmüller <dennis. brandmueller@stud.fh-rosenheim.de>
2013-06-16 23:23:05 +0200

    - animationsbewegung bei blocked event verbessert - holzerarmee hinzugefügt

--- Project-Pokemon/src/de/pokemon/ResourceManager.java
+++ Project-Pokemon/src/de/pokemon/ResourceManager.java
@@ -21,6 +21,8 @@ public class ResourceManager {
 		npcDatabase.put("biker", new NpcStruct("biker", "res/biker.png", new int[][]{{6,3},{5,4},{8,3},{7,4}}));
 		npcDatabase.put("captain", new NpcStruct("captain", "res/captain.png", new int[][]{{3,0},{0,2147483647}}));
 		npcDatabase.put("deubler1", new NpcStruct("deubler1", "res/deubler.png", new int[][]{{1,0},{0,1000},{4,0},{0,1000},{3,0},{0,1000},{4,0},{0,1000}}));
+		npcDatabase.put("holzerarmee", new NpcStruct("holzerarmee", "res/holzerarmee.png", new int[][]{{0,2147483647}}));
+		
 	}
 	
 	public static String getNpcImagePath(String name){
