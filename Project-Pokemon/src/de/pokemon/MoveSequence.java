a7c54dec62dfad70b4aed3379d09b19a539730d0
Dennis Brandmüller <dennis. brandmueller@stud.fh-rosenheim.de>
2013-06-17 00:10:06 +0200

    - enum Klasse eingeführt, sodass Bewegungsmuster der Npc's besser zu lesen ist

---
+++ Project-Pokemon/src/de/pokemon/MoveSequence.java
@@ -0,0 +1,17 @@
+package de.pokemon;
+
+public class MoveSequence {
+	
+	public static enum moveSequence{
+		wait,
+		turnLeft,
+		turnUp,
+		turnRight,
+		turnDown,
+		moveLeft,
+		moveUp,
+		moveRight,
+		moveDown;
+	}
+
+}
