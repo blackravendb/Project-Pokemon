90630fc048bd4fae13d85cf287156e4d19974b6e
Lukas Schmitt <lukas.a.schmitt@gmail.com>
2013-07-01 22:32:20 +0200

    minor change

--- Project-Pokemon/src/de/pokemon/Map.java
+++ Project-Pokemon/src/de/pokemon/Map.java
@@ -50,12 +50,13 @@ public class Map extends TiledMapPlus{
 	 *
 	 */
 	private void buildCollisionMap() {
-		blocked = new boolean[getWidth()][getHeight()];
 		int layerIndex = getLayerIndex("collision");
-
+		int tileID = -1;
+		blocked = new boolean[getWidth()][getHeight()];
+		
 		for (int x = 0; x < getWidth(); x++) {
 			for (int y = 0; y < getHeight(); y++) {
-				int tileID = getTileId(x, y, layerIndex);
+				tileID = getTileId(x, y, layerIndex);
 				blocked[x][y] = getTileProperty(tileID, "blocked", "false").equals("true");
 			}
 		}
